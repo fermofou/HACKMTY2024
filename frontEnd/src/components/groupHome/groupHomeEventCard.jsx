7446529f4a01271bf3e6510fbb28a86ee5834d62
fermofou <fermofou@gmail.com>
2024-09-15 09:40:11 -0600

    quick fix ez

--- frontEnd/src/components/groupHome/groupHomeEventCard.jsx
+++ frontEnd/src/components/groupHome/groupHomeEventCard.jsx
@@ -2,48 +2,47 @@ import PersonImage from "../../assets/personImage.png";
 
 import "./GroupHomeEventCard.css";
 
-function EventCard({ event }) {
-    // Formating deadline
-    const deadline = event.deadline;
-    const date = new Date(deadline);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options);
+function GroupHomeEventCard({ event }) {
+  // Formating deadline
+  const deadline = event.deadline;
+  const date = new Date(deadline);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
 
-    const porcentage = Math.round(event.balance * 100 / event.goal, 0);
+  const porcentage = Math.round((event.balance * 100) / event.goal, 0);
 
-    const formattedGoal = event.goal.toLocaleString('en-US');
-    const formattedBalance = event.balance.toLocaleString('en-US');
+  const formattedGoal = event.goal.toLocaleString("en-US");
+  const formattedBalance = event.balance.toLocaleString("en-US");
 
-    console.log(porcentage);
+  console.log(porcentage);
 
-    console.log(event);
-    return (
-        <>
-            <div className="event-card-container">
-                <div className="event-card-top">
-                    <p>
-                        {event.name}
-                    </p>
-                    <p>
-                        {formattedDate}
-                    </p>
-                </div>
-                <div className="event-card-middle">
-                    <h2>${formattedBalance}<span> / {formattedGoal}</span></h2>
-                </div>
-                <div className="event-card-bottom">
-                    <div className="progress-container">
-                        <progress value={porcentage} max={100}></progress>
-                        <span>{porcentage}%</span>
-                    </div>
-                    <div>
-                        <p>{event.participants.length}</p>
-                        <img src={PersonImage} alt="image person" />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  console.log(event);
+  return (
+    <>
+      <div className="event-card-container">
+        <div className="event-card-top">
+          <p>{event.name}</p>
+          <p>{formattedDate}</p>
+        </div>
+        <div className="event-card-middle">
+          <h2>
+            ${formattedBalance}
+            <span> / {formattedGoal}</span>
+          </h2>
+        </div>
+        <div className="event-card-bottom">
+          <div className="progress-container">
+            <progress value={porcentage} max={100}></progress>
+            <span>{porcentage}%</span>
+          </div>
+          <div>
+            <p>{event.participants.length}</p>
+            <img src={PersonImage} alt="image person" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default EventCard;
+export default GroupHomeEventCard;
--- frontEnd/src/pages/groupHome/GroupHome.jsx
+++ frontEnd/src/pages/groupHome/GroupHome.jsx
@@ -1,54 +1,63 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import ContributionCard from "../../components/groupHome/ContributionCard";
-import EventCard from "../../components/groupHome/GroupHomeEventCard.jsx";
+import GroupHomeEventCard from "../../components/groupHome/GroupHomeEventCard.jsx";
 import ParticipantCard from "../../components/groupHome/ParticipantCard";
-import { url } from '../../assets/constants/constants';
+import { url } from "../../assets/constants/constants";
 
 import "./GroupHome.css";
 
 function GroupHome() {
-    const { groupId } = useParams();
-    const [eventDetail, setEventDetail] = useState(null);
-    const userId = localStorage.getItem('selectedUserId');
+  const { groupId } = useParams();
+  const [eventDetail, setEventDetail] = useState(null);
+  const userId = localStorage.getItem("selectedUserId");
 
-    async function fetchEventDetails() {
-        try {
-            const response = await axios.get(url + `event/${groupId}`);
-            setEventDetail(response.data);
-        } catch (error) {
-            console.log("Error fetching event" + error);
-        }
+  async function fetchEventDetails() {
+    try {
+      const response = await axios.get(url + `event/${groupId}`);
+      setEventDetail(response.data);
+    } catch (error) {
+      console.log("Error fetching event" + error);
     }
-    
-    useEffect(() => {
-        const interval = setInterval(() => {
-            fetchEventDetails();
-        }, 1000);
-        fetchEventDetails();
+  }
 
-        return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetchEventDetails();
+    }, 1000);
+    fetchEventDetails();
 
-    console.log(eventDetail);
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <>
-            {eventDetail ? 
-                <div className='group-home-container'>
-                    <EventCard event={eventDetail}></EventCard>
-                    <ContributionCard event={eventDetail} event_id={groupId} account_id={userId}/>
-                    <div className='group-home-participant-container'>
-                    {eventDetail.participants.map((participant, index) => (
-                        <ParticipantCard key={index} participant={participant} goal={eventDetail.goal}/>
-                    ))}
-                    </div>
-                </div>
-                
-            : <></>}
-        </>
-    )
+  console.log(eventDetail);
+
+  return (
+    <>
+      {eventDetail ? (
+        <div className="group-home-container">
+          <GroupHomeEventCard event={eventDetail}></GroupHomeEventCard>
+          <ContributionCard
+            event={eventDetail}
+            event_id={groupId}
+            account_id={userId}
+          />
+          <div className="group-home-participant-container">
+            {eventDetail.participants.map((participant, index) => (
+              <ParticipantCard
+                key={index}
+                participant={participant}
+                goal={eventDetail.goal}
+              />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 export default GroupHome;
