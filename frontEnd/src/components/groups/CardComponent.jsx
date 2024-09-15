58a2e2375685f076d0107260a3c7a76893546dce
fermofou <fermofou@gmail.com>
2024-09-15 11:30:53 -0600

    5

--- frontEnd/src/components/groups/CardComponent.jsx
+++ frontEnd/src/components/groups/CardComponent.jsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import CapitalOneLogo from '../../assets/CapitalOneLogo_2.png'
-import './CardComponent.css';  // Import the CSS file
+import React from "react";
+import CapitalOneLogo from "../../assets/CapitalOneLogo_2.png";
+import "./CardComponent.css"; // Import the CSS file
 
-const CardComponent  = ({ title, cardNumber, expirationDate, cvv}) =>  {
+const CardComponent = ({ title, cardNumber, expirationDate, cvv }) => {
   return (
-    <div className="card">
-        <div className='card-img-div'>
-            <img src={CapitalOneLogo} className='card-logo-img'></img>
+    <div className="cardCont">
+      <div className="card">
+        <div className="card-img-div">
+          <img src={CapitalOneLogo} className="card-logo-img"></img>
         </div>
-    
-        <div className='card-information'>
-            <div className="card-details">
-                <p><b>{title}</b></p>
-                <p>{expirationDate}</p>
-            </div>
 
-            <div className="card-details">
-                <p>{cardNumber}</p>
-                <p>{cvv}</p>
-            </div>
+        <div className="card-information">
+          <div className="card-details">
+            <p>
+              <b>{title}</b>
+            </p>
+            <p>{expirationDate}</p>
+          </div>
+
+          <div className="card-details">
+            <p>{cardNumber}</p>
+            <p>{cvv}</p>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
