5bbfdef52533ce34300cbfb9468a4a9d6e8ce927
JulenHopp <112414883+JulenHopp@users.noreply.github.com>
2024-09-14 22:40:42 -0600

    updates

--- frontEnd/src/pages/groupHome/GroupHome.jsx
+++ frontEnd/src/pages/groupHome/GroupHome.jsx
@@ -1,17 +1,34 @@
-import { useParams} from 'react-router-dom';
-import ContributionCard from '../../components/groups/ContributionCard'; 
-
-import "./GroupHome.css"
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { url }from '../../assets/constants/constants'
 
 function GroupHome() {
     const { groupId } = useParams();
+
+    const [eventDetail, setEventDetail] = useState([]);
+
+    useEffect(() => {
+        async function fetchEventDetails() {
+            try {
+                const response = await axios.get(url + `event/${groupId}`);
+                setEventDetail(response.data);
+            } catch (error) {
+                console.log("Error fetching event" + error);
+            }
+        }
+
+        fetchEventDetails();
+    }, []);
+
+    console.log(eventDetail);
 
     return (
         <>
             <div>
                 group home {groupId}
+                <ContributionCard/>
             </div>
-            <ContributionCard/>
         </>
     )
 }
