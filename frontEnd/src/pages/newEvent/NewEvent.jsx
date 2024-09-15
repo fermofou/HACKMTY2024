fb6dfe27eb1d6aac05b55470363067a6e19b8780
fermofou <fermofou@gmail.com>
2024-09-14 21:30:26 -0600

    updated front

--- frontEnd/src/pages/newEvent/NewEvent.jsx
+++ frontEnd/src/pages/newEvent/NewEvent.jsx
@@ -1,11 +1,9 @@
 import UpperNavbar from "../../components/upperNavbar/UpperNavbar";
 import EventForm from "../../components/newEvent/EventForm";
+
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import EventCard from "../../components/groups/EventCard";
-import CapitalOneLogo from "../../assets/CapitalOneLogo.png";
-import PlusImg from "../../assets/createNewEventPlusImg.png";
 
 import "./NewEvent.css";
 
@@ -13,7 +11,7 @@ function NewEvent() {
   return (
     <>
       <UpperNavbar text={"Create Event"} type={"none"} />
-      <div>
+      <div className="screen">
         {/* <h1>Create Event</h1> */}
         <EventForm />
       </div>
