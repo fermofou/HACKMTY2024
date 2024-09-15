207dd074787d9888bc837a3d7f30435b327b8fe4
A01722540 <a01722540@tec.mx>
2024-09-14 21:11:48 -0600

    GroupCard y CardComponent creation, my first commit XOXO los quiere -Gabo <3

---
+++ frontEnd/src/components/groups/CardComponent.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import CapitalOneLogo from '../../assets/CapitalOneLogo.png'
+import './CardComponent.css';  // Import the CSS file
+
+const CardComponent  = ({ title, cardNumber, expirationDate, cvv}) =>  {
+  return (
+    <div className="card">
+        <div className='card-img-div'>
+            <img src={CapitalOneLogo} className='card-logo-img'></img>
+        </div>
+    
+        <div className='card-information'>
+            <div className="card-details">
+                <p><b>{title}</b></p>
+                <p>{expirationDate}</p>
+            </div>
+
+            <div className="card-details">
+                <p>{cardNumber}</p>
+                <p>{cvv}</p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default CardComponent;
--- frontEnd/src/pages/groupCard/GroupCard.jsx
+++ frontEnd/src/pages/groupCard/GroupCard.jsx
@@ -1,12 +1,58 @@
 import { useParams} from 'react-router-dom';
+import CardComponent from '../../components/groups/CardComponent'; 
+import { useState} from 'react'
+
+import "./GroupCard.css"
 
 function GroupCard() {
     const { groupId } = useParams();
+    
+    const [cardData] = useState({
+        balance: 130433,
+        card_number: "6338 9041 9249 8584",
+        expiry_date: "11 / 24",
+        cvv: "772",
+        name: "Barcelona Trip"
+      });
+      
+    const formattedBalance = new Intl.NumberFormat().format(cardData.balance);
+
+    const [transactions] = useState([
+        ["450", "Taxi from Airport"],
+        ["300", "Vodafone SIM Card"],
+        ["1,200", "Hotel Reservation"],
+        ["150", "Coffee at Starbucks"],
+        ["500", "Dinner at La Boqueria"],
+        ["200", "Museum Tickets"]
+      ]);
 
     return (
         <>
-            <div>
-                group card {groupId}
+            <CardComponent
+                title={cardData.name}
+                cardNumber={cardData.card_number}
+                expirationDate={cardData.expiry_date}
+                cvv={cardData.cvv}
+            />
+
+            <div className='balance-card'>
+                <p className='balance-label'>Balance</p>
+                <p className='balance-amount'>$ {formattedBalance}</p>
+            </div>
+
+            <div className='add-wallet-btn'>
+                Add to Wallet
+            </div>
+            
+            <div className='transactions'>
+                <p>Transactions</p>
+                <ul>
+                    {transactions.map((transaction, index) => (
+                    <li key={index}>
+                        Ammount: $ {transaction[0]} <br/> Description: {transaction[1]} <br/> <p>Report Purchase</p>
+                    </li>
+                    ))}
+                </ul>
             </div>
         </>
     )
