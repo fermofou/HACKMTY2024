add0af0f5ace4f1515efd7d7c3a9cfaec93326f2
JulenHopp <112414883+JulenHopp@users.noreply.github.com>
2024-09-14 20:41:03 -0600

    finalizing upper navbar

--- frontEnd/src/components/upperNavbar/UpperNavbar.jsx
+++ frontEnd/src/components/upperNavbar/UpperNavbar.jsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom"
+import Arrow from "../../assets/arrow.png"
+import EventImg from "../../assets/eventImg.png";
+import SavingsImg from "../../assets/savingsImg.png";
 
 import "./UpperNavbar.css"
 
-function UpperNavbar({text}) {
+function UpperNavbar({text, type}) {
+    let typeImg;
+
+    if (type === "none") {
+        typeImg = "none";
+
+    } else if (type === "savings") {
+        typeImg = SavingsImg;
+
+    } else {
+        typeImg = EventImg;
+    }
 
     return (
         <>
-            <div>
-                <Link to='/groups'>go back</Link>
+            <div className="upper-navbar-container">
+                <Link to='/groups'><img src={Arrow} alt="arrow" /></Link>
+                <p>{text}</p>
+                {typeImg === "none" ? "" : <img src={typeImg} alt="type image" />}
             </div>
         </>
     );
