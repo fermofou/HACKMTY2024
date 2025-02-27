88ee3292faca8bde8293f2e9f5f0d694e0a38f0e
fermofou <fermofou@gmail.com>
2025-02-27 10:03:31 -0600

    fixed

--- backEnd/schemas/PersonSchema.js
+++ backEnd/schemas/PersonSchema.js
@@ -1,10 +1,15 @@
-import mongoose, {Schema, model} from 'mongoose';
+import mongoose, { Schema, model } from "mongoose";
 
-export const personSchema = new Schema({
-  balance: Number,
-  first_name: String,
-  last_name: String
-})
+export const personSchema = new Schema(
+  {
+    balance: Number,
+    first_name: String,
+    last_name: String,
+  },
+  {
+    collection: "people", // Explicitly set the collection name to 'people'
+  }
+);
 
-const Person = model('Person', personSchema);
+const Person = model("Person", personSchema);
 export default Person;
--- frontEnd/src/assets/constants/constants.js
+++ frontEnd/src/assets/constants/constants.js
@@ -1,1 +1,2 @@
-export const url = "https://hackmty2024.onrender.com/";
+//export const url = "https://hackmty2024.onrender.com/";
+export const url = "http://localhost:3000/";
